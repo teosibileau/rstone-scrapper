e40200de4592ffc931523abadbd8763e4614055b
teo <teo.sibileau@gmail.com>
2012-06-10 19:32:13 -0300

    adding celery dependencies

--- rstone/settings_default.py
+++ rstone/settings_default.py
@@ -114,6 +114,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
+    'djcelery',
     'albums',
     'top',
     'south',
@@ -150,3 +151,15 @@ LOGGING = {
         },
     }
 }
+
+# Celery related
+
+BROKER_URL= 'redis://localhost:6379/'
+
+CELERY_RESULT_BACKEND = "redis"
+CELERY_REDIS_HOST = "localhost"
+CELERY_REDIS_PORT = 6379
+CELERY_REDIS_DB = 0
+
+import djcelery
+djcelery.setup_loader()
